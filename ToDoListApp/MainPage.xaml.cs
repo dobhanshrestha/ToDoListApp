06db6d9c526fb29eb32d27e3dc66dd7a6e9ae1fe
Dobhan Shrestha <dovanshrestha@GMAIL.COM>
2025-01-28 19:17:31 +0545

    Add project files.

---
+++ ToDoListApp/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ToDoListApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<TaskItem> Tasks { get; set; } = new ObservableCollection<TaskItem>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            TaskListView.ItemsSource = Tasks;
+        }
+
+        private void OnAddTaskClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(NewTaskEntry.Text))
+            {
+                Tasks.Add(new TaskItem { Name = NewTaskEntry.Text, IsCompleted = false });
+            }
+        }
+
+        private void OnDeleteTaskClicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            var task = button?.BindingContext as TaskItem;
+
+            if(task != null)
+            {
+                Tasks.Remove(task);
+            }
+        }
+    }
+
+}
